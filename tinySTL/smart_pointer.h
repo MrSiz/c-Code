2a7d19c42611637e25b1ee3e25d0942d5ecbbda8
MrSiz <zhmssl@outlook.com>
2017-03-14 14:27:14 +0800

    smart_pointer

---
+++ tinySTL/smart_pointer.h
@@ -0,0 +1,16 @@
+
+namespace tinySTL
+{
+	template <class T>
+	class shared_ptr
+	{
+		
+	};
+	
+	
+	template <class T, class Deleter = tinySTL::default_delete<T>> 
+	class unique_ptr
+	{
+	
+	};
+}
